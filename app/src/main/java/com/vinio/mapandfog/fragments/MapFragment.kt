5735b0f5dcd6e4eaac2e9aee05910932fa4f5050
Vinio <Ewq@1605>
2025-02-07 23:20:24 +0300

    [ADD] location point

--- app/src/main/java/com/vinio/mapandfog/fragments/MapFragment.kt
+++ app/src/main/java/com/vinio/mapandfog/fragments/MapFragment.kt
@@ -1,16 +1,25 @@
 package com.vinio.mapandfog.fragments
 
+import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.vinio.mapandfog.databinding.FragmentMapBinding
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.layers.ObjectEvent
 import com.yandex.mapkit.map.CameraPosition
 import com.yandex.mapkit.mapview.MapView
+import com.yandex.mapkit.user_location.UserLocationLayer
+import com.yandex.mapkit.user_location.UserLocationObjectListener
+import com.yandex.mapkit.user_location.UserLocationView
+
 
 class MapFragment : Fragment() {
     private var _binding: FragmentMapBinding? = null
@@ -47,12 +56,65 @@ class MapFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mapView = binding.mapView
+        requestLocationPermission()
+    }
 
-        // Устанавливаем позицию камеры (Москва)
-        val targetLocation = Point(55.751244, 37.618423)
-        mapView.mapWindow.map.move(
-            CameraPosition(targetLocation, 10.0f, 0.0f, 0.0f)
-        )
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        val fineLocationGranted = permissions[Manifest.permission.ACCESS_FINE_LOCATION] ?: false
+        val coarseLocationGranted = permissions[Manifest.permission.ACCESS_COARSE_LOCATION] ?: false
+
+        if (fineLocationGranted || coarseLocationGranted) {
+            initializeMap()
+        } else {
+            Toast.makeText(requireContext(), "Разрешение на местоположение не получено!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            initializeMap()
+        } else {
+            locationPermissionRequest.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
+    }
+
+    private fun initializeMap() {
+        // Проверяем, что карта готова
+        if (::mapView.isInitialized) {
+            // Настроим карту и позицию камеры
+            val targetLocation = Point(56.8584, 35.9006)
+            mapView.mapWindow.map.move(
+                CameraPosition(targetLocation, 10.0f, 0.0f, 0.0f)
+            )
+
+            val userLocationLayer = MapKitFactory.getInstance().createUserLocationLayer(mapView.mapWindow)
+            userLocationLayer.isVisible = true
+            userLocationLayer.isHeadingEnabled = true
+            userLocationLayer.setObjectListener(object : UserLocationObjectListener {
+                override fun onObjectAdded(userLocationView: UserLocationView) {
+                    // Настройка внешнего вида метки местоположения пользователя
+                }
+
+                override fun onObjectUpdated(userLocationView: UserLocationView, event: ObjectEvent) {
+                    // Обработка обновлений местоположения пользователя
+                }
+
+                override fun onObjectRemoved(userLocationView: UserLocationView) {
+                    // Обработка удаления метки местоположения пользователя
+                }
+            })
+
+        } else {
+            Toast.makeText(requireContext(), "Ошибка инициализации карты!", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onStop() {
